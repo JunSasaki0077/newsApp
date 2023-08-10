f15488234db64027e9ca2cb94bcae43784ecd208
JunSasaki0077 <ikasas0077@gmail.com>
2023-08-10 17:50:06 +0900

    ⚡️ 自動更新追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Articles } from "./components/articles";
 export default async function Home() {
   const API = process.env.NEWS_API;
   const res = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=5&apiKey=${API}`
+    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=5&apiKey=${API}`, { next: { revalidate: 60 * 10 } }
   );
   const json = await res.json();
   const articles = json?.articles;
--- src/app/topics/[id]/page.tsx
+++ src/app/topics/[id]/page.tsx
@@ -9,7 +9,7 @@ type Props = {
 async function getPost(params: Props) {
   const API = process.env.NEWS_API;
   const topicRes = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=5&category=${params.id}&country=jp&apiKey=${API}`
+    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=5&category=${params.id}&country=jp&apiKey=${API}`, { next: { revalidate: 60 * 10 } }
   );
   const topicJson = await topicRes.json();
   const topicArticles = await topicJson.articles;
