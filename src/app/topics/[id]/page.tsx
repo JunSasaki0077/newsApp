d96fdf32165def27d01d65c50f31a04dc01e4a05
JunSasaki0077 <ikasas0077@gmail.com>
2023-08-10 12:05:02 +0900

    ⚡️記事表示件数変更

--- src/app/topics/[id]/page.tsx
+++ src/app/topics/[id]/page.tsx
@@ -9,7 +9,7 @@ type Props = {
 async function getPost(params: Props) {
   const API = process.env.NEWS_API;
   const topicRes = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=jp&category=${params.id}&country=jp&apiKey=${API}`
+    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=5&category=${params.id}&country=jp&apiKey=${API}`
   );
   const topicJson = await topicRes.json();
   const topicArticles = await topicJson.articles;
